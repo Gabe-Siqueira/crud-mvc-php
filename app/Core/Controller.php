305839ef95a30b279fbabe362a41839362753108
Gabe-Siqueira <gabriel.snsilva2@gmail.com>
2022-12-03 06:28:19 -0300

    create Controller core

---
+++ app/Core/Controller.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\Core;
+
+class Controller
+{
+    /**
+    * method returns model
+    *
+    * @return string
+    */
+    public function model($model)
+    {
+        require '../app/Models/' . $model . '.php';
+        $class = 'app\\Models\\' . $model;
+        return new $class();
+    }
+
+    /**
+    * method returns view
+    *
+    * @return string
+    * @return array
+    */
+    public function view(string $view, $data = [])
+    {
+        require '../app/Views/' . $view . '.php';
+    }
+
+    /**
+    * method return view erro404
+    *
+    */
+    public function pageNotFound()
+    {
+        $this->view('erro404');
+    }
+}
