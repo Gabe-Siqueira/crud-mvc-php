447b51702df7384c3da0f2e5b4be0197059478d9
Gabe-Siqueira <gabriel.snsilva2@gmail.com>
2022-12-03 19:02:34 -0300

    create view provider index

--- app/Views/provider/index.php
+++ app/Views/provider/index.php
@@ -1,29 +1,87 @@
+<?php
+
+$message = '';
+if(isset($data['message'])){
+    switch ($data['message']) {
+        case 'success':
+        $message = '<div class="alert alert-success">Ação executada com sucesso!</div>';
+        break;
+
+        case 'error':
+        $message = '<div class="alert alert-danger">Ação não executada!</div>';
+        break;
+    }
+}
+
+$result = '';
+foreach ($data['provider'] as $value) {
+    $result .=  "<tr class='table-active' id='".$value['id']."'>
+                    <th>".$value['id']."</th>
+                    <td>".$value['name']."</td>
+                    <td>".date('d/m/Y à\s H:i:s',strtotime($value['date_register']))."</td>
+                    <td class='text-center' >
+                        <div class='row' style='display: inline-flex;'>
+                            <a class='btn btn-primary btn-sm mr-1' href='http://localhost:8080/provider/edit/".$value['id']."' role='button'>
+                                Editar <i class='fa fa-pencil-square-o' aria-hidden='true'></i>
+                            </a>
+                            <a class='btn btn-danger btn-sm' href='http://localhost:8080/provider/delete/".$value['id']."' role='button'>
+                                Excluir <i class='fa fa-trash-o' aria-hidden='true'></i>
+                            </a>
+                        </div>
+                    </td>
+                </tr>";
+}
+
+$result = strlen($result) ? $result : '<tr>
+                                            <td colspan="5" class="text">
+                                                Nenhum dado encontrado.
+                                            </td>
+                                        </tr>';
+
+?>
+
 <main class="main">
     <section class="jumbotron text-center" style="margin: 0px;">
         <div class="container">
             <h1 class="jumbotron-heading">Fornecedor</h1>
+            <p>
+                <a href="http://localhost:8080/home/index" class="btn btn-secondary my-2">Voltar</a>
+                <a href="http://localhost:8080/provider/create" class="btn btn-success my-2">Cadastrar</a>    
+            </p>
         </div>
     </section>
 
-    <div class="album py-5 bg-light">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="card mb-4 shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                        <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+    <div class="container mt-5 mb-5">
+        <div class="row">
+            <div class="col-12">
+                <?=$message?>
+                <!-- INCIO CARD LISTA PROVIDER -->
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="table-responsive-sm  table-responsive-md mt-3">
+                                    <table id="providerDataTable" class="table table-striped table-bordered no-wrap">
+                                        <thead>
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Nome</th>
+                                                <th>Data Cadastro</th>
+                                                <th>Ações</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?=$result?>
+                                        </tbody>
+                                    </table>
                                 </div>
-                                <small class="text-muted">9 mins</small>
                             </div>
                         </div>
                     </div>
                 </div>
+                <!-- FIM CARD LISTA PROVIDER -->
             </div>
         </div>
     </div>
+
 </main>
