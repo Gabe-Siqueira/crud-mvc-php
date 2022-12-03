af6259968b21000deff6e580275fec6e98c37f9f
Gabe-Siqueira <gabriel.snsilva2@gmail.com>
2022-12-03 19:01:22 -0300

    crete view error 404

---
+++ app/Views/error/404.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="row">
+        <div class="col-12 text-center" style="margin-top:100px;margin-bottom:100px;">
+            <img src="/assets/img/erro.png">
+        </div>
+    </div>
+</div>
