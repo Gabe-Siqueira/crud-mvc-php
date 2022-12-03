928606e782cbf2de822993e8a715d793063f2c4e
Gabe-Siqueira <gabriel.snsilva2@gmail.com>
2022-12-03 18:59:32 -0300

    update provider controller -> add function store, update and delete

--- app/Controllers/ProviderController.php
+++ app/Controllers/ProviderController.php
@@ -4,11 +4,11 @@ use app\Core\Controller;
 
 class ProviderController extends Controller
 {
-    public function index()
+    public function index($message = null)
     {
         $providerModel = $this->model('Provider');
         $provider = $providerModel::index();
-        $this->view('provider/index', ['provider' => $provider]);
+        $this->view('provider/index', ['provider' => $provider, 'message' => $message]);
     }
 
     public function create()
@@ -16,14 +16,52 @@ class ProviderController extends Controller
         $this->view('provider/create');
     }
 
+    public function store()
+    {
+        try {
+            $name = $_POST['name'];
+            $providerModel = $this->model('Provider');
+            $providerModel::store($name);
+            $this->view('provider/create', ['message' => 'success']);
+        } catch (\Exception $e) {
+            $this->view('provider/create', ['message' => 'error']);
+        }
+        
+    }
+
     public function edit($id = null)
     {
         if (is_numeric($id)) {
             $providerModel = $this->model('Provider');
-            $provider = $providerModel::findById($id);
+            $provider = $providerModel::show($id);
             $this->view('provider/edit', ['provider' => $provider]);
         }else{
             $this->pageNotFound();
+        }
+    }
+
+    public function update($id = null)
+    {
+        try {
+            $name = $_POST['name'];
+            $providerModel = $this->model('Provider');
+            $providerModel::update($name, $id);
+            $provider = $providerModel::show($id);
+            $this->view("provider/edit", ['provider' => $provider, 'message' => 'success']);
+        } catch (\Exception $e) {
+            $this->view("provider/edit", ['provider' => $provider,'message' => 'error']);
+        }
+    }
+
+    public function delete($id = null)
+    {
+        try {
+            $providerModel = $this->model('Provider');
+            $providerModel::delete($id);
+            $provider = $providerModel::index($id);
+            $this->view("provider/index", ['provider' => $provider, 'message' => 'success']);
+        } catch (\Exception $e) {
+            $this->view("provider/index", ['provider' => $provider, 'message' => 'error']);
         }
     }
 }
