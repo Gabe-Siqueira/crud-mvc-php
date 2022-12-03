791f58b722bec25e44b8303cbe53bd83b9d41a07
Gabe-Siqueira <gabriel.snsilva2@gmail.com>
2022-12-03 11:48:57 -0300

    update public index

--- public/index.php
+++ public/index.php
@@ -1,21 +1,44 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
   <head>
     <meta charset="utf-8">
-    <title>MVC PHP</title>
+    <title>Teste Dev</title>
     <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
   </head>
+
   <body>
+    <header>
+      <div class="navbar navbar-dark bg-dark shadow-sm">
+        <div class="container d-flex justify-content-between">
+            <a href="#" class="navbar-brand d-flex align-items-center">
+                <strong>Sistema Fornecedor e Produto</strong>
+            </a>
+            <nav class="nav nav-masthead justify-content-center">
+                <a class="nav-link text-white" href="provider/index">Fornecedor</a>
+                <a class="nav-link text-white" href="#">Produto</a>
+            </nav>
+        </div>
+      </div>
+    </header>
 
-  <?php
-    require '../app/autoload.php';
+    <?php
+      require '../app/autoload.php';
 
-    use app\Core\App;
+      use app\Core\App;
 
-    $app = new App();
+      $app = new App();
 
-  ?>
-  <script src="/assets/js/jquery.slim.min.js"></script>
-  <script src="/assets/js/bootstrap.min.js"></script>
+    ?>
+
+    <footer class="footer mt-auto py-3 bg-dark text-center">
+      <div class="container">
+          <span class="text-white text-muted">Teste Dev. CRUD MVC PHP.</span>
+      </div>
+    </footer>
+
+    <script src="/assets/js/jquery.slim.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
   </body>
+
 </html>
